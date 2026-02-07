16deb11c65e38bfa6ecb915572ca399b7b284a42
mithi17 <mithileshkumar170203@gmail.com>
2026-02-07 10:50:58 +0530

    chore: configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/mithilesh-portfolio",
+  base: "/myportfolio/",
 });
